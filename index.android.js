57a2bb399051a9297766a48c9a65c27621b1bf94
Keerthana Kumar <keerthana.kumar@gmail.com>
2018-08-24 20:08:36 -0700

    Update to project dir

--- index.android.js
+++ index.android.js
@@ -10,7 +10,26 @@ class HelloUser extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.hello}>Hey! How are you?</Text>
+        <Text style={styles.license}>The MIT License (MIT)
+        Copyright 2014 Oleksandr Melnykov
+
+        Permission is hereby granted, free of charge, to any person obtaining a copy
+        of this software and associated documentation files (the "Software"), to deal
+        in the Software without restriction, including without limitation the rights
+        to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+        copies of the Software, and to permit persons to whom the Software is
+        furnished to do so, subject to the following conditions:
+
+        The above copyright notice and this permission notice shall be included in all
+        copies or substantial portions of the Software.
+
+        THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+        IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+        FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+        AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+        LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+        OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+        SOFTWARE.</Text>
       </View>
     )
   }
@@ -20,7 +39,7 @@ var styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
   },
-  hello: {
+  licence: {
     fontSize: 20,
     textAlign: 'center',
     margin: 10,
