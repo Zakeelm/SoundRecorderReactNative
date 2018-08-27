c1cd817a1d1fdeb52df60486626c6c36a1a8351b
Keerthana Kumar <keerthana.kumar@gmail.com>
2018-08-27 17:20:10 -0400

    Integrated codepush

---
+++ android-old/SoundRecorder/app/src/main/java/com/danielkim/soundrecorder/activities/MainActivity.java
@@ -0,0 +1,131 @@
+package com.danielkim.soundrecorder.activities;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.provider.Settings;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+//import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.astuetz.PagerSlidingTabStrip;
+import com.danielkim.soundrecorder.R;
+import com.danielkim.soundrecorder.fragments.FileViewerFragment;
+import com.danielkim.soundrecorder.fragments.LicensesFragment;
+import com.danielkim.soundrecorder.fragments.RecordFragment;
+
+
+public class MainActivity extends AppCompatActivity{
+
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+    private final int OVERLAY_PERMISSION_REQ_CODE = 1;  // Choose any value
+    private PagerSlidingTabStrip tabs;
+    private ViewPager pager;
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        pager = (ViewPager) findViewById(R.id.pager);
+        pager.setAdapter(new MyAdapter(getSupportFragmentManager()));
+        tabs = (PagerSlidingTabStrip) findViewById(R.id.tabs);
+        tabs.setViewPager(pager);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setPopupTheme(R.style.ThemeOverlay_AppCompat_Light);
+        if (toolbar != null) {
+            setSupportActionBar(toolbar);
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (!Settings.canDrawOverlays(this)) {
+                Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                        Uri.parse("package:" + getPackageName()));
+                startActivityForResult(intent, OVERLAY_PERMISSION_REQ_CODE);
+            }
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        // Handle presses on the action bar items
+
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                Intent i = new Intent(this, SettingsActivity.class);
+                startActivity(i);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == OVERLAY_PERMISSION_REQ_CODE) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (!Settings.canDrawOverlays(this)) {
+                    Toast.makeText(this, "You cannot open the React Native app as you have denied the permission", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+
+    public class MyAdapter extends FragmentPagerAdapter {
+        private String[] titles = { getString(R.string.tab_title_record),
+                getString(R.string.tab_title_saved_recordings) };
+
+        public MyAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch(position){
+                case 0:{
+                    return RecordFragment.newInstance(position);
+                }
+                case 1:{
+                    return FileViewerFragment.newInstance(position);
+                }
+            }
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            return titles.length;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return titles[position];
+        }
+    }
+
+    public MainActivity() {
+    }
+
+
+}
